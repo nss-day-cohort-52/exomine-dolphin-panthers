6bb864167c12e362c14df469bab2c24446bda955
Jubjub422 <ross.stovenour@gmail.com>
2021-10-26 14:32:52 -0500

    governor eventlistener and set function

---
+++ scripts/Governors.js
@@ -0,0 +1,13 @@
+
+
+
+//Event listener for governor select
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if(changeEvent.target.id === "governor"){
+            
+        }
+    }
+
+)
