67269212174aae3351c428be6bae8863d66b4923
Sean Derevjanik <sean.derevjanik@gmail.com>
2021-10-26 15:07:23 -0700

    comments

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -20,7 +20,7 @@ export const Governors = () => {
     let html = `
     <select id="select__governor">
     <option value="0">---Select a governor---</option>`
-    for (const governor of governors) {
+    for (const governor of governors) { //loops through governors and constructs a series of option for the select field
         html += `<option value="${governor.id}"`
         if (transientstate.selectedGovernor === governor.id) html += " selected" //rather than playing with =true or false, I just added the selected attribute only if the case is true
         html += `>${governor.name}</option>` 
