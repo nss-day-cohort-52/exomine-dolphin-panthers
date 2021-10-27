68826bd75c9027d5420fea4d72b4c9f76612b1aa
Sean Derevjanik <sean.derevjanik@gmail.com>
2021-10-27 09:01:21 -0700

    added radio button for selected facility

--- scripts/Facilities.js
+++ scripts/Facilities.js
@@ -36,19 +36,20 @@ export const FacilityMinerals = () => {
     const facilities = getFacilities()
     const minerals = getMinerals()
     let facilityMinerals = getFacilityMinerals()
-    const displayedFacility = facilities.find((facility) => (transientstate.selectedFacility === facility.id)) 
+    const displayedFacility = facilities.find((facility) => (transientstate.selectedFacility === facility.id)) //checks for displayed facility
     let html = "<ul>"
-    if(transientstate.selectedFacility !== undefined){facilityMinerals = facilityMinerals.filter((facility) => displayedFacility.id === facility.facilityId)
-    facilityMinerals.map(
+    if(transientstate.selectedFacility !== undefined){facilityMinerals = facilityMinerals.filter((facility) => (displayedFacility.id === facility.facilityId)) //if prevents errors from reading undefined id. then filters the facilitiesMinerals to only return minerals of the selected facility
+    facilityMinerals.map( //for each mineral present at the given facility
         (facMineral) => {
-            const minName = minerals.find((mineral) => facMineral.mineralId === mineral.id)
-            html += `<li>
-                <input type="radio" name="${minName}" value="${facMineral.mineralId}" /> ${minName}:${facMineral.mineralQuanitity}
-                </li>`
+            const minName = minerals.find((mineral) => facMineral.mineralId === mineral.id) //find its name based on the id
+            html += `<li> 
+                <input type="radio" name="${minName.mineralName}" value="${facMineral.mineralId}" /> ${facMineral.mineralQuanitity} tons of ${minName.mineralName}
+                </li>` //construct an html line-item. current setup limits minerals to 1 of each type per purchase. can remove "name" field to enable multi-purchase. event listen would target "type" in that case, since we only have one set of radio buttons
         }
     )}
+    else html += ""
     html += "</ul>"
-    
+    return html
 
 }
 
